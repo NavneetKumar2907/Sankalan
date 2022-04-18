98fab1b0eb4efa274f6d07f7059f67959977f397
rishi <singhrishi245021@gmail.com>
2022-04-18 21:21:20 +0530

    created myEvent fragment and team member in a recycler view with adapter etc.

---
+++ app/src/main/java/com/example/sankalan/data/MyEvents.kt
@@ -0,0 +1,8 @@
+package com.example.sankalan.data
+
+data class MyEvents( val myEventName: String) {
+    var teamMember1 : String = "NONE"
+    var teamMember2 : String ="NONE"
+    var teamMember3 : String ="NONE"
+    var teamMember4 : String ="NONE"
+}
