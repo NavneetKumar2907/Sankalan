d9e6b9800dd1a4d45a00e796ee5193ac742e531c
shivpujan12 <shivpujanyadav12@gmail.com>
2022-04-25 19:28:05 +0530

    Updated the LoginActivity UI

--- app/src/main/java/com/example/sankalan/activities/LoginActivity.kt
+++ app/src/main/java/com/example/sankalan/activities/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.example.sankalan.activities
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.sankalan.R
 import com.example.sankalan.databinding.LoginActivityBinding
 
 
@@ -15,6 +18,11 @@ class LoginActivity : AppCompatActivity(){
 
         binding = LoginActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val animDrawable = findViewById<ConstraintLayout>(R.id.rootLayout).background as AnimationDrawable?
+        animDrawable?.setEnterFadeDuration(10)
+        animDrawable?.setExitFadeDuration(3000)
+        animDrawable?.start()
 
     }
 
